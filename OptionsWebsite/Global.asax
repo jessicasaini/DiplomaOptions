fe0e1c07e1ff3d908470a4c45fc74cd834a80b97
JessicaSaini <jessicasaini@hotmail.com>
2016-02-13 23:49:35 +0000

    first commit of template

---
+++ OptionsWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OptionsWebsite.MvcApplication" Language="C#" %>
---
+++ OptionsWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OptionsWebsite.Startup))]
+namespace OptionsWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
