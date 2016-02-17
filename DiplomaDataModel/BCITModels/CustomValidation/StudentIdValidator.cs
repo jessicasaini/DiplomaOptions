78482ed45d5d4606f1fa7337fef2f3d9b4ef4ca3
JessicaSaini <jessicasaini@hotmail.com>
2016-02-17 19:19:38 +0000

    added titles

---
+++ DiplomaDataModel/BCITModels/CustomValidation/StudentIdValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace MVC01.Models.CustomValidation
+{
+    public class StudentIdValidator : ValidationAttribute
+    {
+
+        public StudentIdValidator() : base("Id must start with A00 and be 9 characters long")
+        {
+           
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            if (value != null)
+            {
+
+                char[] delimiters = new char[] { ' ', '\r', '\n' };
+                var stringVal = value.ToString();
+                int x = stringVal.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+
+                if (false)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
